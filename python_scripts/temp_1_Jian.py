1ce3c81a0d9720b223c0ba8f060abf07eda8be76
Kevin Jian Luo <jluo@MSDITs-MacBook-Air.local>
2021-01-25 11:50:22 +0000

    Exercise script

---
+++ python_scripts/temp_1_Jian.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Exercise 1
+x = 5
+
+y = "Python exercises"
+
+z = True
+
+print (x, y, z)
+
+# Exercise 2
+x = 1
+below_11 = True
+while below_11:
+    print (x)
+    x = x + 1
+    if x == 11:
+        below_11 = False
+    
+    else:
+        continue
+      
+# Exercise 3
+start, end = -10, 0
+for x in range (start, end):
+    if x < 0:
+        print (x, end = " ")
+
+# Exercise 3
+for i in range (-10,0):
+    print (i)
+    
+# Exercise 4
+n = input ('Enter a number : ')
+x = sum (1,n)
+print (x)
